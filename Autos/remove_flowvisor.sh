fe08f752591015409131208659c0f70809a158fd
nykykof-natasha <nykykof@gmail.com>
2017-02-01 22:20:34 +0100

    "application web"

---
+++ Autos/remove_flowvisor.sh
@@ -0,0 +1,5 @@
+apt remove --purge flowvisor
+rm -rf /etc/flowvisor
+rm -rf /usr/share/db/flowvisor
+rm -rf /var/log/flowvisor
+rm -rf $HOME/GPG-*
---
+++ Autos/topo.sh
@@ -0,0 +1,1 @@
+sudo mn --custom $HOME/mininet/custom/neteam.py --topo=neteam --controller=remote --link=tc --arp --mac
