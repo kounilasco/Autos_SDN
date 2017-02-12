6d70a9beeba5ab2adf2767e5ae2270d418540439
nykykof-natasha <nykykof@gmail.com>
2017-02-12 21:18:31 +0100

    Autos

--- remove_mininet.sh
+++
@@ -1,7 +0,0 @@
-rm -rf $HOME/openflow
-rm -rf $HOME/oftest
-rm -rf $HOME/oflops
-rm -rf $HOME/pox
-rm -rf $HOME/mininet
-rm -rf $HOME/loxigen
-rm -rf $HOME/GPG-*
--- topo.sh
+++
@@ -1,1 +0,0 @@
-sudo mn --custom $HOME/mininet/custom/neteam.py --topo=neteam --controller=remote --link=tc --arp --mac
